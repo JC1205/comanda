2e9f4cfb2dbf3bdc22e6d55c7d20455838976d7d
camachotrue <luisorlandocamacho55@gmail.com>
2025-03-01 16:58:58 -0700

    primer commit

---
+++ src/Home.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="home-container">
+      <button class="comedor-button" @click="">Comedor</button>
+      <button class="domicilio-button" @click="">Domicilio</button>
+      <button class="cerrar-sesion-button"@click="cerrarSesion">Cerrar sesión</button>
+    </div>
+  </template>
+  
+  <script setup>
+  import { useRouter } from 'vue-router';
+  
+  const router = useRouter();
+  
+  const cerrarSesion = () => {
+    router.push('/'); //  Regresa al Login
+  };
+  </script>
+
+<style>
+
+.comedor-button {
+  background-color: rgb(223, 246, 77);
+  color: rgb(0, 0, 0); 
+  border: 10;
+  padding: 25px 10px;
+  font-size: 12px;
+  border-radius: 15px; 
+  cursor: pointer; 
+}
+
+.domicilio-button {
+  background-color: rgb(223, 246, 77);
+  color: rgb(0, 0, 0); 
+  border: 10;
+  padding: 25px 10px;
+  font-size: 12px;
+  border-radius: 15px; 
+  cursor: pointer; 
+}
+
+.cerrar-sesion-button {
+  background-color: rgb(223, 246, 77);
+  color: rgb(0, 0, 0); 
+  border: 10;
+  padding: 25px 10px;
+  font-size: 12px;
+  border-radius: 15px; 
+  cursor: pointer; 
+}
+
+.home-container {
+  background-color: #81817f; 
+  padding: 20px; 
+}
+
+</style>
+  
---
+++ src/Login.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="login-container">
+        <img src="C:\Users\gamer\PioDesk\pio.png" alt="Logo">
+        <h1>Login</h1>
+        <hr/>
+        <h2>User</h2>
+        <input type="text" v-model="usuario" placeholder="Ingresa tu usuario">
+        <h2>Password</h2>
+      
+        <input type="password" v-model="password" placeholder="Ingresa tu Contraseña">
+        <h2></h2>
+        <button @click="iniciarSesion">Iniciar sesión</button>
+        
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const usuario = ref('');
+const password = ref('');
+
+
+// Prueba de usuarios
+const usuarios = [
+    { username: 'admin', password: '1234' }
+];
+
+const iniciarSesion = () => {
+    const esValido = usuarios.some(u => u.username === usuario.value && u.password === password.value);
+
+    if (esValido) {
+        router.push('/home');
+    } else {
+        console.log('Usuario o contraseña incorrectos');
+        router.push('/')
+    }
+};
+
+</script>
