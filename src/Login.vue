6b572943b460f59268d55b8cc4327ed778aad4cd
DaatKruby <kgsk@outlook.es>
2025-03-01 17:11:20 -0700

    arreglé el importe de imagen pio

--- src/Login.vue
+++ src/Login.vue
@@ -1,16 +1,15 @@
 <template>
     <div class="login-container">
-        <img src="C:\Users\gamer\PioDesk\pio.png" alt="Logo">
+        <img src="../pio.png" alt="Logo">
         <h1>Login</h1>
         <hr/>
         <h2>User</h2>
         <input type="text" v-model="usuario" placeholder="Ingresa tu usuario">
-        <h2>Password</h2>
-      
+        <h2>Password</h2>    
         <input type="password" v-model="password" placeholder="Ingresa tu Contraseña">
         <h2></h2>
         <button @click="iniciarSesion">Iniciar sesión</button>
-        
+    
     </div>
 </template>
 
