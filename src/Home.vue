337b24fe519786c2f0f5c0145780901dcdc08132
JC1205 <josecacp12@gmail.com>
2025-03-07 12:40:24 -0700

    Diseño de login y abrir turni sin tailwind

--- src/Home.vue
+++ src/Home.vue
@@ -9,6 +9,9 @@
     <button @click="salir">Salir</button>
   </div>
 
+  <div class="container">
+    <img src="/PIO2.jpeg" alt="">
+  </div>
   <AbrirTurno :mostrar="mostrarVentana" @cerrar="mostrarVentana = false" />
 </template>
 
@@ -37,20 +40,39 @@ const salir = () => {
 }
 
 button {
-  background-color: #ffcc00;
+  background-color: rgb(247, 219, 75);
   border: none;
   padding: 15px;
   min-width: 100px;
+  height: 65px;
   border-radius: 10px;
-  font-size: 14px;
+  font-size: 13px;
   font-weight: bold;
   cursor: pointer;
   box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.2);
   text-align: center;
+  transition: background-color 0.3s ease,transform 0.2s ease;
+}
+
+.container{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80vh;
+}
+
+img{
+  width: 500px;
+  max-width: 100%;
+  max-height: 100%;
 }
 
 button:hover {
-  background-color: #e6b800;
+  transform: scale(1.07);
+}
+
+button:active{
+  background-color: white;
 }
 
 .salir-btn {
--- src/Login.vue
+++ src/Login.vue
@@ -1,15 +1,12 @@
 <template>
-    <div class="login-container">
-        <img src="../pio.png" alt="Logo">
-        <h1>Login</h1>
-        <hr/>
-        <h2>User</h2>
-        <input type="text" v-model="usuario" placeholder="Ingresa tu usuario">
-        <h2>Password</h2>    
-        <input type="password" v-model="password" placeholder="Ingresa tu Contraseña">
-        <h2></h2>
+    <div class="login-bg">
+        <div class="login-container">
+            <img src="../PIO.jpg" alt="Logo">
+            <h1>Bienvenido!</h1>
+            <input type="text" v-model="usuario" placeholder="Usuario">
+            <input type="password" v-model="password" placeholder="Contraseña">
         <button @click="iniciarSesion">Iniciar sesión</button>
-    
+        </div>
     </div>
 </template>
 
@@ -31,7 +28,7 @@ const iniciarSesion = async () => {
         .eq("userName", usuario.value)
         .single();  // Si solo hay un usuario con ese nombre
 
-       
+
     if (error) {
        console.error("Error al obtener usuario:", error.message);
         return;
@@ -49,27 +46,72 @@ const iniciarSesion = async () => {
 </script>
 
 <style scoped>
-.login-container {
-    max-width: 300px;
-    margin: 0 auto;
-    padding: 20px;
-    border-radius: 10px;
-    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+*{
+    margin:  0;
+    padding:  0;
+    box-sizing: border-box;
+    font-family: Arial, Helvetica, sans-serif;
+}
+.login-bg{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgb(214, 214, 214);
+    height: 100vh;
+}
+.login-container{
+    background-color: rgb(255, 255, 255);
+    width: 430px;
+    height: 430px;
+    box-shadow: 2px 5px 10px rgba(0, 0, 0, 0.2);
     text-align: center;
+    border-radius: 10px;
+
 }
-input {
-    width: 100%;
-    padding: 8px;
-    margin: 10px 0;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-}
-button {
-    background-color: #ffcc00;
-    border: none;
-    padding: 10px;
-    width: 100%;
-    border-radius: 5px;
+
+button{
+    background-color: rgb(247, 219, 75);
+    height: 45px;
+    width: 300px;
+    border-radius: 13px;
+    border: 0;
+    font-size: 15px;
+    margin-top: 20px;
+    transition: background-color 0.3s ease, transform 0.2s ease;
     cursor: pointer;
+
+}
+
+button:hover {
+    transform:  scale(1.02);
+}
+
+button:active{
+    background-color: rgb(255, 255, 255);
+}
+
+input{
+    height: 45px;
+    width: 300px;
+    margin-top: 20px;
+    padding-left: 10px;
+
+}
+
+img{
+    width: 80px;
+    margin-top: 35px;
+}
+
+h2{
+    font-weight: 700;
+    font-size: 35px;
+    margin-top: 5px;
+
+    
+}
+
+::placeholder{
+    font-size: 15px;
 }
 </style>
