4d1f8543ff8642154d13d8e6848dcb1166018c52
Abm2717 <abm55812767@gmail.com>
2025-03-07 03:49:33 -0700

    cambios de supabase

--- src/supabase/supabase.ts
+++ src/supabase/supabase.ts
@@ -1,3 +1,3 @@
 import { createClient } from '@supabase/supabase-js'
-
-export const supabaseClient = createClient('https://<project>.supabase.co', '<your-anon-key>')
+//Pa conectar con la base de datos
+export const supabaseClient = createClient('https://umzgodcmuwmkajlnvnju.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtemdvZGNtdXdta2FqbG52bmp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEzMzY4NTEsImV4cCI6MjA1NjkxMjg1MX0.RrP3RY9Dv7fYER0XpzrW9flYR0kNtW2uipvGPOfrGhg')  
